46da11b040b6d7b0e7f3ece5df12ddb78cd06012
AEmpire <18751294896@163.com>
2017-05-04 21:01:50 +0800

    添加项目文件。

---
+++ Compiler/main.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using Compiler.Common;
+using Compiler.Scanner;
+using Compiler.Parser;
+
+namespace Compiler
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            parser Parser = new parser();
+
+            TreeNode root = Parser.getTree( "../../source.txt" );
+            Console.WriteLine( "EROOR? " + Parser.error.isError );
+            Parser.DislayTokenList();
+            //Display( root );
+            Console.ReadKey();
+            
+        }
+        static public void Display(TreeNode root)
+        {
+            Console.Write( root.NonTerminal + ":  " );
+            for (int i = 0; i < root.ChildNum; i++)
+            {
+                try
+                {
+                    if (root.childs[i].IsTerminal) Console.Write(root.childs[i].Terminal + "    ");
+                    else Console.Write(root.childs[i].NonTerminal + " ");
+                }
+                catch (NullReferenceException)
+                {
+                    Console.WriteLine(" ");
+                    return;
+                    ;
+                }
+            
+            }
+            Console.WriteLine( "" );
+            for (int i = 0; i < root.ChildNum; i++)
+            {
+                if (root.childs[i].IsTerminal == false) Display( root.childs[i] );
+            }
+        }
+    }
+}
